875478d1db63ade3c40a48d1a17be1a4b171701a
Serhii-Ter <serhij86@gmail.com>
2022-08-09 23:55:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -383,23 +383,53 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const products = [
-  { name: 'Radar', price: 1300, quantity: 4 },
-  { name: 'Scanner', price: 2700, quantity: 3 },
-  { name: 'Droid', price: 400, quantity: 7 },
-  { name: 'Grip', price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
 
-function getProductPrice(productName) {
-  for (const product of products) {
-    if (product.name === productName) {
-      return product.price;
-    }
-  }
-  return null;
-}
-console.log(getProductPrice('Radar'));
-console.log(getProductPrice('Grip'));
-console.log(getProductPrice('Scanner'));
-console.log(getProductPrice('Droid'));
-console.log(getProductPrice('Engine'));
+// function getProductPrice(productName) {
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+// console.log(getProductPrice('Radar'));
+// console.log(getProductPrice('Grip'));
+// console.log(getProductPrice('Scanner'));
+// console.log(getProductPrice('Droid'));
+// console.log(getProductPrice('Engine'));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const allPropValues = [];
+//   for (const product of products) {
+//     const props = Object.keys(product);
+//     for (const prop of props) {
+//       if (prop === propName) {
+//         allPropValues.push(product[prop]);
+//       }
+//     }
+//   }
+
+//   return allPropValues;
+// }
+
+// console.log(getAllPropValues('name'));
+// console.log(getAllPropValues('quantity'));
+// console.log(getAllPropValues('price'));
+// console.log(getAllPropValues('category'));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
