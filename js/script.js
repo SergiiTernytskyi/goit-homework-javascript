bd352ff1fc3c3a6e29b3bfad6b16a54926471cb5
Sergii Ternytskyi <serhij86@gmail.com>
2022-08-10 18:07:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -433,3 +433,24 @@
 // console.log(getAllPropValues('category'));
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  let totalPrice = 0;
+  for (const product of products) {
+    if (product.name === productName) {
+      totalPrice = product.price * product.quantity;
+    }
+  }
+  return totalPrice;
+}
+console.log(calculateTotalPrice('Blaster'));
+console.log(calculateTotalPrice('Radar'));
+console.log(calculateTotalPrice('Droid'));
+console.log(calculateTotalPrice('Grip'));
+console.log(calculateTotalPrice('Scanner'));
